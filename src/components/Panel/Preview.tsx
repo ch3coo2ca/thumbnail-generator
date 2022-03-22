31b8a595e6bf8f3eb70f96b77aa60dbb8a73d04b
jylee <wjddp3@naver.com>
2022-03-22 22:47:15 +0900

    feat : add configuration preview panel

---
+++ src/components/Panel/Preview.tsx
@@ -0,0 +1,34 @@
+import {StyleConfig} from 'models/Editor';
+
+import styled from 'styled-components';
+
+const PreviewContainer = styled.div`
+  width: 100%;
+  max-width: 700px;
+  height: 500px;
+  max-height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2em;
+  box-sizing: content-box;
+`;
+
+const Text = styled.div`
+  text-align: center;
+  word-break: break-word;
+  max-height: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const Preview: React.FC<StyleConfig> = (props) => {
+  const {backgroundColor, font, fontSize, text, textColor} = props;
+  return (
+    <PreviewContainer style={{backgroundColor}}>
+      <Text style={{fontSize, color: textColor, fontFamily: font}}>{text}</Text>
+    </PreviewContainer>
+  );
+};
+
+export default Preview;
---
+++ src/stories/panel/Preview.stories.tsx
@@ -0,0 +1,55 @@
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+
+import {FONT_SIZE_LIST, DEFAULT_FONTS, DEFAULT_CONFIG} from 'constants/App';
+import Preview from 'components/Panel/Preview';
+
+export default {
+  title: 'components/Panel/Preview',
+  component: Preview,
+  argTypes: {
+    backgroundColor: {
+      description: 'background color',
+      table: {
+        summary: 'string'
+      }
+    },
+    fontSize: {
+      description: 'font size',
+      table: {
+        summary: 'number'
+      },
+      control: {
+        type: 'range'
+      }
+    },
+    font: {
+      description: 'font type',
+      table: {
+        summary: 'string'
+      },
+      options: DEFAULT_FONTS,
+      control: {
+        type: 'select'
+      }
+    },
+    text: {
+      description: 'text value'
+    },
+    textColor: {
+      description: 'text color'
+    }
+  }
+} as ComponentMeta<typeof Preview>;
+
+const Template: ComponentStory<typeof Preview> = (args) => (
+  <Preview {...args} />
+);
+
+export const Base: ComponentStory<typeof Preview> = Template.bind({});
+Base.args = DEFAULT_CONFIG;
+
+export const WithLongText: ComponentStory<typeof Preview> = Template.bind({});
+WithLongText.args = {
+  ...Base.args,
+  text: 'longlonglonglonglonglong text this is really long'
+};
