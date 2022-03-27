bcd9375f7445e7c75d23b23df6560d67a1ba3316
jylee <wjddp3@naver.com>
2022-03-27 17:36:11 +0900

    style : change default color in preview

--- src/constants/App.ts
+++ src/constants/App.ts
@@ -12,7 +12,7 @@ export const DEFAULT_FONTS = [
 export const FONT_SIZE_LIST = [60, 70, 80, 90, 100] as const;
 
 export const DEFAULT_CONFIG: StyleConfig = {
-  backgroundColor: '#7d9a7d',
+  backgroundColor: '#aed581',
   textColor: 'white',
   font: DEFAULT_FONTS[0],
   fontSize: 60,
